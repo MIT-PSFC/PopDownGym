035c63cd16046143dfe5b7176073607e1cc0a284
Allen Wang <allenw@mit.edu>
2023-11-17 12:53:24 -0500

    Fix test_geom.py

--- pop_down_gym/test/test_geom.py
+++ pop_down_gym/test/test_geom.py
@@ -1,24 +1,32 @@
 import jax
-import pytest
+import jax.numpy as jnp
 
 from pop_down_gym.geometry import Geometry
 from pop_down_gym.load_data import load_data
 
-
-@pytest.mark.skip(reason="test is currently broken TODO@allen-adastra")
 def test_geom():
-    ds = load_data()
-    time = ds.time.values.squeeze()
-    aminor = ds.aminor.values.squeeze()
-    kappa_a = ds.kappa_a.values.squeeze()
-    Vp = ds.Vp.values.squeeze()
+    ds, ds_geom = load_data()
+    time = ds_geom.time.values.squeeze()
+    aminor = ds_geom.aminor.values.squeeze()
+    kappa_a = ds_geom.kappa_a.values.squeeze()
+    Vp = ds_geom.Vp.values.squeeze()
     g = Geometry(1.85, time, aminor, kappa_a, Vp)
     gdotfun = jax.jacfwd(g)
-    gdot = gdotfun(0.0)
 
-    # Could we have anything more informative here?
-    assert gdot is not None
+    gval = g(0.0)
+    gdotval = gdotfun(0.0)
 
+    # Sanity check values of gval.
+    assert gval["aminor"] > 0.5 and gval["aminor"] < 0.6
+    assert gval["kappa_a"] > 1.5 and gval["kappa_a"] < 1.8
+    assert gval["Vp"].size > 20
+    assert gval["Vp"][0] > 0.0 and gval["Vp"][0] < 1.0 # dV/drho near core is small.
+    assert gval["Vp"][-1] > 30.0 and gval["Vp"][-1] < 40.0 # dV/drho near edge is large.
+    assert gval["volume"] > 19.0 and gval["volume"] < 21.0
 
-if __name__ == "__main__":
-    test_geom()
+    # Sanity check values of gdotval.
+    assert gdotval["aminor"] > 0.0 and gdotval["aminor"] < 0.1 # Expect aminor to increase a bit temporarily.
+    assert gdotval["kappa_a"] < 0.0
+    assert gdotval["Vp"].size > 20
+    assert jnp.all(gdotval["Vp"] < 0.0) # Expect Vp to decrease everywhere.
+    assert gdotval["volume"] < 0.0 # Expect volume to decrease.
