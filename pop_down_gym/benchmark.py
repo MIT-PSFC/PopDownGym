58d91651024a26f3e4fe4ea902ffa2972ff4fd65
Allen Wang <allenw@mit.edu>
2023-11-25 14:55:19 -0500

    1) Use block_until_ready to make sure async dispatch doesn't bite us, 2) compare JIT and after JIT, 3) some improvements on plot styling

--- pop_down_gym/benchmark.py
+++ pop_down_gym/benchmark.py
@@ -16,6 +16,7 @@ def benchmark_simulate(batch_sizes: List[int], num_trials: int = 100):
     # Scan over a range of vmap dimensions
     num_steps = 100
     results = []
+    results_burn_in = []
     for batch_size in tqdm(batch_sizes):
         # Define random controls
         prng_key, action_key = jax.random.split(prng_key)
@@ -30,50 +31,63 @@ def benchmark_simulate(batch_sizes: List[int], num_trials: int = 100):
         def fn(key, action):
             return env.simulate_trajectory_open_loop(key, action, num_steps)
         
+        start = time.perf_counter()
         fn = jax.jit(jax.vmap(fn))
-        fn(sim_keys, actions)
+        jax.block_until_ready(fn(sim_keys, actions))
+        burn_in_time = time.perf_counter() - start
+        results_burn_in.append(
+            {
+                "batch_size": batch_size,
+                "num_steps": num_steps,
+                "eval_time": burn_in_time,
+            }
+        )
 
         # Run trials
         for _ in range(num_trials):
             start = time.perf_counter()
-            fn(sim_keys, actions)
+            jax.block_until_ready(fn(sim_keys, actions))
             eval_time = time.perf_counter() - start
 
             log = {
                 "batch_size": batch_size,
                 "num_steps": num_steps,
                 "num_trials": num_trials,
-                "variant": "jit-vmap-scan",
                 "eval_time": eval_time,
             }
             results.append(log)
 
     results = pd.DataFrame(results)
-    return results
+    results_burn_in = pd.DataFrame(results_burn_in)
+    return results, results_burn_in
 
 if __name__ == "__main__":
     batch_sizes = [2**i for i in range(20)]
-    results = benchmark_simulate(batch_sizes, num_trials=1)
+    results, results_burn_in = benchmark_simulate(batch_sizes, num_trials=10)
     import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    sns.set_style("whitegrid")
+    plt.rcParams.update({'font.size': 14})
 
     # Add a column to the data to be evaluation time per sample
     results["Time per trajectory (s)"] = results.eval_time / results.batch_size
 
     # Plot the results of evaluation time per sample vs batch size
     fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-    sns.lineplot(
-        data=results,
-        x="batch_size",
-        y="eval_time",
-        hue="variant",
-        ax=ax,
+    ax.loglog(
+        results.batch_size,
+        results.eval_time,
+        label="After JIT",
     )
-    ax.set_xscale("log")
-    ax.set_yscale("log")
+    ax.loglog(
+        results_burn_in.batch_size,
+        results_burn_in.eval_time,
+        label="JIT",
+    )
+    ax.set_xlabel("Batch size")
+    ax.set_ylabel("Wall-clock Time (s)")
     ax.set_ylim(top=100)
-
+    ax.set_title("Benchmarking PopDownGym")
+    ax.legend()
+    plt.grid(True, which="both", ls="-")
+    
     # Save the figure
-    fig.savefig("benchmarking/gpu_benchmark_simulate_stateless.png", dpi=300)
+    fig.savefig("gpu_benchmark_simulate_stateless.png", dpi=300)
