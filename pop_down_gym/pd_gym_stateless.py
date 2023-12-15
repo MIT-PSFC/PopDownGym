24e7a5c55aa97b3b6ee605c894fbd5c71a9bd59e
Oswin So <oswinso@gmail.com>
2023-12-15 13:13:55 -0500

    remove eixt

--- pop_down_gym/pd_gym_stateless.py
+++ pop_down_gym/pd_gym_stateless.py
@@ -48,9 +48,6 @@ class PopDownGymStateless:
         self.action_space = jnp.vstack(
             (-1.0 * np.ones(len(self.action_ranges)), np.ones(len(self.action_ranges)))
         )
-        
-        print(repr(list(self.action_ranges.keys())))
-        exit(0)
 
         # Declare the normalized observation space.
         self.observation_space = {
